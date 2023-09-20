b7482ac796f7cd5a25a1fbd9bf77613db6c35f9d
Yiqun Huang <albert913388672@gmail.com>
2023-09-20 16:19:10 -0700

    add middle.js

---
+++ middle.js
@@ -0,0 +1,48 @@
+// TEST/ASSERTION FUNCTIONS
+const eqArrays = function(actualArr, expectedArr) {
+  if (actualArr.length !== expectedArr.length) {
+    return false;
+  }
+  for (let i = 0; i < actualArr.length; i++) {
+    if (actualArr[i] !== expectedArr[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(actualArr, expectedArr) {
+  if (eqArrays(actualArr, expectedArr)) {
+    console.log(`✅✅✅ Assertion Passed: ${actualArr} === ${expectedArr}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actualArr} !== ${expectedArr}`);
+  }
+};
+
+
+// ACTUAL FUNCTION
+const middle = function(array) {
+  //...
+  const result = [];
+  if (array.length <= 2) {
+    return result;
+  }
+
+  if (array.length % 2 === 0) {
+    result.push(array[array.length / 2 - 1]);
+    result.push(array[array.length / 2]);
+  } else {
+    result.push(array[Math.floor(array.length / 2)]);
+  }
+  return result;
+};
+
+// TEST CODE
+// ...
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
