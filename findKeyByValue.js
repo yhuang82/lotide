398bfd36923107075600899e50736b0bbebfa807
Yiqun Huang <albert913388672@gmail.com>
2023-09-21 01:12:21 -0700

    add findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,34 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = (inputObject, givenvalue) => {
+  const valuesArr = Object.values(inputObject);
+  if (valuesArr.includes(givenvalue)) {
+    for (let key in inputObject) {
+      if (inputObject[key] === givenvalue) {
+        return key;
+      }
+    }
+  } else {
+    return undefined;
+  }
+};
+
+
+
+const bestTVShowsByGenre = {
+  "sci_fi": "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
