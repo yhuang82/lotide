3c95ddd513b4509687b2fdcaaa857dc5d127aeac
Yiqun Huang <albert913388672@gmail.com>
2023-09-21 23:14:26 -0700

    add finKey.js

---
+++ findKey.js
@@ -0,0 +1,37 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+
+
+const findKey = (inputObj, callback) => {
+  let result = undefined;
+  for (let key in inputObj) {
+    if (callback(inputObj[key])) {
+      result = key;
+      break;
+    }
+  }
+  return result;
+};
+
+
+
+
+const test = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+assertEqual(test, "noma");
