f66cb8c9337a50384037ad51c1be24c9fb6f1def
Yiqun Huang <albert913388672@gmail.com>
2023-09-21 21:12:06 -0700

    add map.js

---
+++ map.js
@@ -0,0 +1,51 @@
+const eqArrays = function(actualArr, expectedArr) {
+  if (actualArr.length !== expectedArr.length) {
+    return false;
+  }
+  for (let i = 0; i < actualArr.length; i++) {
+    if (actualArr[i] !== expectedArr[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+const assertArraysEqual = function(actualArr, expectedArr) {
+  if (eqArrays(actualArr, expectedArr)) {
+    console.log(`✅✅✅ Assertion Passed: ${actualArr} === ${expectedArr}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actualArr} !== ${expectedArr}`);
+  }
+};
+
+
+
+
+
+
+
+
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+const nums = [1, 2, 3, 4];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+const results1 = map(words, word => word[0]);
+const results2 = map(nums, num => num * num);
+const results3 = map(words, word => word.length);
+
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
+assertArraysEqual(results2, [1, 4, 9, 16]);
+assertArraysEqual(results3, [6, 7, 2, 5, 3]);
+
+
+
+
