4cdf9f3844614424e49dd0247b0e42c7159aa5f4
Yiqun Huang <albert913388672@gmail.com>
2023-09-25 15:52:21 -0700

    modify the tail and write a tail test for it

--- tail.js
+++ tail.js
@@ -1,26 +1,8 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const tail = function(arr) {
+const tail = function (arr) {
   if (arr.length <= 1) {
     return [];
   }
   return arr.slice(1);
 };
 
-
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// Test Case 2: ...
-// ...
-
+module.exports = tail;
