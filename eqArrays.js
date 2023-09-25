29f0afc2dc0a692353e7034ea6d6f7355029cc95
Yiqun Huang <albert913388672@gmail.com>
2023-09-25 16:12:36 -0700

    modify the eqArrays and write a test file for eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,17 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp"); // This will fail
-// assertEqual(1, 1); // This will pass
-
-
 
 
 
@@ -29,21 +15,9 @@ const eqArrays = function(actualArr, expectedArr) {
   return true;
 };
 
-// TEST CODE
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
-
-console.log(eqArrays([[1,2]], [[1,2]])); // => true
 
 
+module.exports = eqArrays;
 
 
 
---
+++ test/eqArraysTest.js
@@ -0,0 +1,16 @@
+const eqArrays = require("../eqArrays")
+const assertEqual = require("../assertEqual")
+// TEST CODE
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+console.log(eqArrays([[1, 2]], [[1, 2]])); // => true
