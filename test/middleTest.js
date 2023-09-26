b761c812cd9272942f25ef2be96dcb0441b844f6
Yiqun Huang <albert913388672@gmail.com>
2023-09-25 22:36:39 -0700

    edit the index.js file

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,14 +2,6 @@ const { assert } = require("chai");
 const assertArraysEqual = require("../assertArraysEqual")
 const middle = require("../middle")
 // TEST CODE
-// ...
-// assertArraysEqual(middle([1]), []);
-// assertArraysEqual(middle([1, 2]), []);
-// assertArraysEqual(middle([1, 2, 3]), [2]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-
 
 
 describe("#middle", () => {
